8453af32fd555c85028de10bd5f2c012e180455e
SabihaKhanum <sabkhan306@gmail.com>
2024-11-05 15:35:01 +0530

    sentiment analysis

--- feedback_analysis/feedback/admin.py
+++ feedback_analysis/feedback/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import CustomerFeedback
 
-# Register your models here.
+class CustomerFeedbackAdmin(admin.ModelAdmin):
+    list_display = ('review', 'sentiment')  # Columns to display in the list view
+    search_fields = ('review',)              # Enable search functionality
+
+admin.site.register(CustomerFeedback, CustomerFeedbackAdmin)
+
+
